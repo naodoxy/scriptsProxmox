a466cdafd3965343f5c10d38d6985220e6ebc0f2
Sarah <100850378+naodoxy@users.noreply.github.com>
2025-07-02 11:15:35 +0200

    Update network_config.sh

--- script/network_config.sh
+++ script/network_config.sh
@@ -4,6 +4,7 @@
 if [ -z "$LXC_NETWORK" ]; then
   read -p "Entrez le réseau (ex: 172.16.1.0/24) : " LXC_NETWORK
   read -p "Entrez la passerelle (gateway) : " LXC_GATEWAY
+  read -p "Entrez le nom de la carte bridge de proxmox (par défaut vmbr0) : " LXC_VMBR
 
   # Découpe le réseau pour en extraire la base IP et CIDR
   LXC_BASE=$(echo "$LXC_NETWORK" | cut -d'.' -f1-3)
@@ -14,5 +15,6 @@ cat <<EOF > /tmp/.network_env
 LXC_BASE=$LXC_BASE
 LXC_CIDR=$LXC_CIDR
 LXC_GATEWAY=$LXC_GATEWAY
+LXC_VMBR=$LXC_VMBR
 EOF
 fi
