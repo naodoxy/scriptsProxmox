81cd2df29fb1277f783eeac752133251af93195b
Sarah <100850378+naodoxy@users.noreply.github.com>
2025-07-03 11:32:53 +0200

    Update powerdnsauthoritative.sh

--- script/powerdnsauthoritative.sh
+++ script/powerdnsauthoritative.sh
@@ -2,6 +2,25 @@
 
 [ -f /tmp/.network_env ] && source /tmp/.network_env
 
+#Fonction de vérification du format des IP
+
+is_valid_ip() {
+  local ip=$1
+  # Vérifie le format global avec regex
+  if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    # Vérifie que chaque octet est entre 0 et 255
+    IFS='.' read -r -a octets <<< "$ip"
+    for octet in "${octets[@]}"; do
+      if ((octet < 0 || octet > 255)); then
+        return 1
+      fi
+    done
+    return 0
+  else
+    return 1
+  fi
+}
+
 # Vérification que le script est exécuté en root
 if [ "$EUID" -ne 0 ]; then
     echo "Ce script doit être exécuté en tant que root."
@@ -48,8 +67,18 @@ while true; do
     fi
 done
 
-read -p "Quelle est l'IP de votre serveur proxmox? [172.16.1.110] : " SERVER1_IP
-SERVER1_IP=${SERVER1_IP:-172.16.1.110}
+while true; do
+  read -p "Quelle est l'IP de votre serveur proxmox ? [172.16.1.110] : " SERVER1_IP
+  SERVER1_IP=${SERVER1_IP:-172.16.1.110}
+
+  if is_valid_ip "$SERVER1_IP"; then
+    break
+  else
+    echo "Erreur : L'adresse IP n'est pas valide. Merci de réessayer."
+  fi
+done
+
+echo "IP validée : $SERVER1_IP"
 
 while true; do
     read -s -p "Quel mot de passe voulez vous pour votre base de donnée de PowerDNS?: " PDNS_DB_PASSWORD
@@ -66,8 +95,18 @@ PDNS_API_KEY=${PDNS_API_KEY}
 read -p "Sur quel port voulez vous accéder à PowerDNS Admin? [9595]: " D_PORT
 D_PORT=${D_PORT:-9595}
 
-read -p "Entrez l'IP du serveur principal [10.1.1.15]: " SERVER_IP
-SERVER_IP=${SERVER_IP:-10.1.1.15}
+while true; do
+  read -p "Entrez l'IP du serveur principal [10.1.1.15]: " SERVER_IP
+  SERVER_IP=${SERVER_IP:-10.1.1.15}
+
+  if is_valid_ip "$SERVER_IP"; then
+    break
+  else
+    echo "Erreur : L'adresse IP n'est pas valide. Merci de réessayer."
+  fi
+done
+
+echo "IP validée : $SERVER_IP
 
 read -p "Entrez le nom de votre zone/domaine [int.com]: " ZONE_NAME
 ZONE_NAME=${ZONE_NAME:-int.com}
